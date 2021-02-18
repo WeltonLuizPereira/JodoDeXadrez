22438da52bc7d92c80d46e9958f3b13f5e920486
Welton <wljpwelton@gmail.com>
2021-02-18 15:48:24 -0300

    INICIANDO O PROJETO

---
+++ XADREZ_CONSOLE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XADREZ_CONSOLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
