5deb22443226ba8c58aa90535cec4d1478b9583e
Welton <wljpwelton@gmail.com>
2021-02-18 16:34:30 -0300

    CRIANDO A POSIÇÃO

--- XADREZ_CONSOLE/Program.cs
+++ XADREZ_CONSOLE/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using XADREZ_CONSOLE.TABULEITO;
 
 namespace XADREZ_CONSOLE
 {
@@ -6,7 +7,8 @@ namespace XADREZ_CONSOLE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine(p);
         }
     }
 }
